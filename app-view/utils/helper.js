efea9749cfe0ed8333d8d10d16310ee6ddc5a080
caigehui <1136687@qq.com>
2017-11-06 19:20:18 +0800

    Update

--- app-view/utils/helper.js
+++ app-view/utils/helper.js
@@ -75,6 +75,7 @@ export function mapLocaleString(arr, keys) {
     return arr;
 }
 
+// form校验函数
 async function validator({value, callback, model, field}) {
     const { data } = await request('/api/verify', { post: { model, field, value } });
     if (data.exist) callback(`${name}已重复，请重新输入`);
